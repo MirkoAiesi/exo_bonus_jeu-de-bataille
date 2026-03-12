305fadcfcbce48ebf0592c6616e5eac17c9dc389
Aiesi Mirko <m.aiesi@stag.technofuturtic.education>
2026-03-12 16:40:59 +0100

    ajout des énumération : ValeurCartes et CouleurCartes

--- exo_bonus_Jeu-de-bataille/Program.cs
+++ exo_bonus_Jeu-de-bataille/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+public enum ValeurCartes
+{
+    Deux,
+    Trois,
+    Quatre,
+    Cinq,
+    Six,
+    Sept,
+    Huit,
+    Neuf,
+    Dix,
+    Valet,
+    Dame,
+    Roi, 
+    As
+}
+
+public enum CouleurCartes
+{
+    Coeur,
+    Carreau,
+    Trefle,
+    Pique
+}
